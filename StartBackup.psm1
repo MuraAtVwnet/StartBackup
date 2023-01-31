aed3907e86b3768d89e08b9cb3ef0578d20155fb
Syuichi Murashima <mura@vwnet.jp>
2023-01-31 18:18:25 +0900

    1st commit

---
+++ StartBackup.psm1
@@ -0,0 +1,9 @@
+﻿# StartBackup
+
+if (-not(([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))) {
+	echo "[FAIL] 実行には管理権限が必要"
+	exit
+}
+
+WBADMIN START BACKUP -addtarget:e: -include:D:,C: -allCritical -vssFull -quiet
+Stop-Computer -Force
